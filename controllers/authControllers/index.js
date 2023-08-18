acfdcd8ebf92a34b360008f80f8ef5680b14f949
yuvraj <yuvrajra@vigorushealth.com>
2023-08-18 15:13:06 +0530

    refresh-token-update

--- controllers/authControllers/index.js
+++ controllers/authControllers/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const signupController = async (req, res) => {
   let { username, password ,role} = req.body;
 
-  console.log({ username, password });
+  // console.log({ username, password });
   try {
     if (!username || !password) {
       return res.status(400).json({ error: "All input fields are required" });
@@ -35,154 +35,124 @@ const signupController = async (req, res) => {
 };
 
 const authController = async (req, res) => {
-const cookies=req.cookies;
+  const cookies = req.cookies;
   const encryptedData = req.body.encryptedData;
-  console.log({encryptedData});
-  const ENCRYPT_SECRET=process.env.ENCRYPT_SECRET
-  // Decrypt the encrypted data using the same secret key and IV
+  const ENCRYPT_SECRET = process.env.ENCRYPT_SECRET;
+  
   const bytes = CryptoJS.AES.decrypt(encryptedData, ENCRYPT_SECRET);
   const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
-
+  
   const userData = JSON.parse(decryptedData);
   const { username, password } = userData;
-  // let { username, password } = req.body;
-  // // console.log({username});
-  // // console.log({password});
+  //  const { username, password } = req.body;
+  
   try {
     if (!username || !password) {
       return res.status(400).json({ error: "All input fields are required" });
     }
-
+  
     const findUser = await User.findOne({ username });
+  
     if (!findUser) {
       return res.status(404).json({ error: "No user found" });
     }
-
+  
     const isPasswordMatch = await bcrypt.compare(password, findUser.password);
+  
     if (!isPasswordMatch) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
- const payload= {
-  id:findUser._id,
-  username:username,
-  role:findUser.role
- }
-//  const expirationTimeInSeconds = parseInt('10', 10);
- const token= generateJwtToken(payload,'1m')
-
- const refreshToken=jwt.sign(
-  { username: findUser.username },
-  process.env.REFRESH_TOKEN_SECRET,
-  { expiresIn: "1d" }
-);
-let newRefreshTokenArray =
-!cookies?.jwt
-    ? findUser.refreshToken
-    : findUser.refreshToken.filter(rt => rt !== cookies.jwt);
+  
+    const payload = {
+      id: findUser._id,
+      username: username,
+      role: findUser.role,
+    };
+  
+    const token = generateJwtToken(payload, '1m');
+  
+    const refreshToken = jwt.sign(
+      { username: findUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+  
+    let newRefreshTokenArray = !cookies?.jwt
+      ? findUser.refreshToken
+      : findUser.refreshToken.filter(rt => rt !== cookies.jwt);
+  
     if (cookies?.jwt) {
-      const refreshToken = cookies.jwt;
-      const foundToken = await User.findOne({ refreshToken }).exec();
+      const refreshTokenFromCookies = cookies.jwt;
+      const foundToken = await User.findOne({ refreshToken: refreshTokenFromCookies }).exec();
+  
       if (!foundToken) {
-          console.log('attempted refresh token reuse at login!')
-          newRefreshTokenArray = [];
+        console.log('Attempted refresh token reuse at login!');
+        newRefreshTokenArray = [];
       }
-
-      res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-  }
-
-  // findUser.refreshToken=refreshToken;
-  findUser.refreshToken = [...newRefreshTokenArray, refreshToken];
-  const result = await findUser.save();
-console.log({result});
- res.cookie("jwt", refreshToken, {
-  httpOnly: true, 
-  secure: true, 
-  sameSite: 'none', 
-  maxAge: 7 * 24 * 60 * 60 * 1000, 
-});
+    }
+  
+    findUser.refreshToken = [...newRefreshTokenArray, refreshToken];
+    const result = await findUser.save();
+    console.log('Refresh token saved:', result);
+  
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  
     res.status(200).json({
       token,
-      // refreshToken,
-      success:"true",
-      message:"Logged in successfully!",
+      success: "true",
+      message: "Logged in successfully!",
     });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal server error" });
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ error: "An error occurred during login" });
   }
+  
 };
 
 
 // refresh token 
 const refresh=async(req,res,next)=>{
-  const cookies=req.cookies;
-  console.log(cookies);
-  if(!cookies?.jwt){
-    return res.status(401).send({message:"Unauthorized"})
-  }
-  
-  const refreshToken=cookies.jwt;
-  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-  const foundUser = await User.findOne({ refreshToken }).exec();
+  const cookies = req.cookies;
 
-  // Detected refresh token reuse!
-  if (!foundUser) {
-      jwt.verify(
-          refreshToken,
-          process.env.REFRESH_TOKEN_SECRET,
-          async (err, decoded) => {
-              if (err) return res.sendStatus(403); //Forbidden
-              console.log('attempted refresh token reuse!')
-              const hackedUser = await User.findOne({ username: decoded.username }).exec();
-              hackedUser.refreshToken = [];
-              const result = await hackedUser.save();
-              console.log(result);
-          }
-      )
-      return res.sendStatus(403); //Forbidden
-  }
-  const newRefreshTokenArray = foundUser.refreshToken.filter(rt => rt !== refreshToken);
-  jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,
+  if (!cookies?.jwt) return res.status(401).json({ message: "Unauthorized" });
+
+  const refreshToken = cookies.jwt;
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
     asyncHandler(async (err, decoded) => {
-      if (err){
-        foundUser.refreshToken = [...newRefreshTokenArray];
-        const result = await foundUser.save();
-        console.log(result);
-        
-      }
-      if (err ) return res.sendStatus(403);
+      if (err) return res.status(403).json({ message: "Forbidden" });
+
       const foundUser = await User.findOne({
-        username: decoded.username,
+       refreshToken
       }).exec();
 
       if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
-      const payload= {
-        username: foundUser.username,
-        role: foundUser.role,
-      }
-      const token = generateJwtToken(
-        payload,
-         "15m" 
+
+      const token = jwt.sign(
+        {
+          UserInfo: {
+            username: foundUser.username
+          },
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: "15m" }
       );
-      const newRefreshToken = jwt.sign(
-        { "username": foundUser.username },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: '1d' }
-    );
-    // Saving refreshToken with current user
-    foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
-    const result2 = await foundUser.save();
-   console.log({result2});
-    // Creates Secure Cookie with refresh token
-    res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
       res.json({ token });
-    }))
+    })
+  ); 
 }
 const whoAmIController = async (req, res) => {
   try {
     const user = req.user;
-console.log(user);
 
     res.status(200).json({ user });
   } catch (error) {
