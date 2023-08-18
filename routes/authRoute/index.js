e6b3e3623c99af9670984b9b294b5fef1f042c13
yuvraj <yuvrajra@vigorushealth.com>
2023-08-18 17:44:47 +0530

    limit

--- routes/authRoute/index.js
+++ routes/authRoute/index.js
@@ -1,9 +1,10 @@
 
 const authControllers=require('../../controllers/authControllers/index');
 const authorizeUser = require('../../middleware/authorizeUser');
+const loginLimiter = require('../../middleware/loginLimitter');
 const router=require('express').Router();
 
-router.post('/login',authControllers.authController)
+router.post('/login',loginLimiter,authControllers.authController)
 router.post('/signup',authControllers.signupController)
 router.get('/refresh',authControllers.refresh)
 router.get('/me',authorizeUser,authControllers.whoAmIController)
