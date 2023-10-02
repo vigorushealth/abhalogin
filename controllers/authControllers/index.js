5bc4d7d4d1f8eee672d93726276251426e1beb7d
yuvrajraghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2023-10-02 16:59:31 +0530

    Update index.js

--- controllers/authControllers/index.js
+++ controllers/authControllers/index.js
@@ -69,7 +69,7 @@ const authController = async (req, res) => {
       role: findUser.role,
     };
   
-    const token = generateJwtToken(payload, '2m');
+    const token = generateJwtToken(payload, '1d');
   
     const refreshToken = jwt.sign(
       { username: findUser.username },
